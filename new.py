54952c9a1bbc2b72d7d354db7c247ffd3ea5b904
vinayagam <you@example.com>
2026-06-02 21:39:58 +0530

    add new program

--- new.py
+++ new.py
@@ -104,27 +104,77 @@
 
 
 
-def find_prime(no):
-    if no>1:
-        div = 2
-        divisors_count = 0
-        while div <= no//2:
-            if no % div == 0: 
-                print('Divisor: ', div)
-                divisors_count+=1
-            div+=1
+##def find_prime(no):
+##    if no>1:
+##        div = 2
+##        divisors_count = 0
+##        while div <= no//2:
+##            if no % div == 0: 
+##                print('Divisor: ', div)
+##                divisors_count+=1
+##            div+=1
+##
+##        if divisors_count == 0:
+##            return True
+##        else:
+##            return False
+##
+##result = find_prime(2)
+##print(result)
+##result = find_prime(13)
+##print(result)
+##result = find_prime(12)
+##print(result)
 
-        if divisors_count == 0:
-            return True
-        else:
-            return False
 
-result = find_prime(2)
-print(result)
-result = find_prime(13)
-print(result)
-result = find_prime(12)
-print(result)
+##--------task--------
+####2.
+##start=1
+##while start<=5:
+##    print (start,end=" ")
+##    start=start+1
 
 
-    
+
+####3.
+##start=1
+##while start<=10:
+##    if start%2 !=0:
+##        print (start)
+##    start=start+1
+
+
+####4.
+##start=3
+##while start<=15:
+##    if start%3==0:
+##        print (start)
+##    start+=1
+
+
+####4.1
+##start=15
+##while start>=1:
+##    if start%3==0:
+##        print(start)
+##    start=start-1
+
+
+####4.2
+##start=10
+##while start>=2:
+##    if start%2==0:
+##        print(start)
+##    start=start-1
+
+
+# ##4.3
+# start=10
+# while start>=1:
+#    if start%2 !=0:
+#        print(start)
+#    start=start-1
+
+
+
+
---
+++ trail.js
@@ -0,0 +1,15 @@
+// let fruits=["apple","orange","mango"]
+// let bag=fruits.map(food=>{
+//   return food+"juice"
+
+// })
+// console.log(bag);
+
+
+// let nums = [1, 2, 3];
+// let result = nums.map((acc, curr) => {
+//   return acc + curr})
+// console.log(result); // [2, 4, 6]
+
+print
+
