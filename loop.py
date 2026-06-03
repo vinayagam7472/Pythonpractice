976a62f235e2c51f051d0b7d2190dbffd21fd42b
vinayagam <you@example.com>
2026-06-03 22:30:03 +0530

    new

--- loop.py
+++ loop.py
@@ -3,3 +3,14 @@ for i in range(1, 51):
         print(i)
 
 
+i = 1
+while i <= 50:
+    if i % 3 == 0 or i % 5 == 0:
+        print(i)
+    i += 1
+
+
+
+for i in range(1, 51):
+    if i % 3 == 0 and i % 5 == 0:
+        print(i)
