db75cf98fc38a7a855f66186e1248dede995900a
vinayagam <you@example.com>
2026-06-03 22:26:39 +0530

    add new task

--- task.py
+++ task.py
@@ -15,32 +15,86 @@
 
 
 ####4.
-##start=3
-##while start<=15:
-##    if start%3==0:
-##        print (start)
-##    start+=1
+start=3
+while start<=15:
+   if start%3==0:
+       print (start)
+   start+=1
 
 
 ####4.1
-##start=15
-##while start>=1:
-##    if start%3==0:
-##        print(start)
-##    start=start-1
+start=15
+while start>=1:
+   if start%3==0:
+       print(start)
+   start=start-1
 
 
 ####4.2
-##start=10
-##while start>=2:
-##    if start%2==0:
-##        print(start)
-##    start=start-1
+start=10
+while start>=2:
+   if start%2==0:
+       print(start)
+   start=start-1
 
 
 # ##4.3
-# start=10
-# while start>=1:
-#    if start%2 !=0:
-#        print(start)
-#    start=start-1
+start=10
+while start>=1:
+   if start%2 !=0:
+       print(start)
+   start=start-1
+
+
+####5.
+start=1
+while start<=50:
+    if start%3==0 and start%5==0:
+        print("divisible by both",start)
+    elif start%3==0:
+        print("divisible by 3",start)
+    elif start%5==0:
+        print("divisible by 5",start)
+    start+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
